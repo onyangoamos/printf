f710fff584b01aa25a240bf2cbe5e407529b478b
onyangoamos <131455400+onyangoamos@users.noreply.github.com>
2024-01-30 14:43:12 -0800

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -14,19 +14,19 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int k, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'k', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	for (k = 0; fmt_types[k].fmt != '\0'; k++)
+		if (fmt[*ind] == fmt_types[k].fmt)
+			return (fmt_types[k].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
+	if (fmt_types[k].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
