63462321159aa0f32d384412a2d88e6c7aa85cb0
onyangoamos <131455400+onyangoamos@users.noreply.github.com>
2024-01-30 14:39:27 -0800

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -3,24 +3,24 @@
 /**
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * @m: List of arguments to be printed.
  *
  * Return: Precision.
  */
-int get_size(const char *format, int *i)
+int get_size(const char *format, int *m)
 {
-	int curr_i = *i + 1;
+	int curr_m = *m + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
+	if (format[curr_m] == 'l')
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+	else if (format[curr_m] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
-		*i = curr_i - 1;
+		*m = curr_m - 1;
 	else
-		*i = curr_i;
+		*m = curr_m;
 
 	return (size);
 }
